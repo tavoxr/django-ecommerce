131bc331f8be08a97d37268ab75b4d2ec02ebc84
tavoxr <tavoxr23@gmail.com>
2021-08-05 10:50:46 -0600

    guestOrder utils

--- store/viewsets/checkout.py
+++ store/viewsets/checkout.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 from ..models import Order, ShippingAddress
 from django.http import  JsonResponse
 import datetime
-from ..utils import cookieCart ,cartData
+from ..utils import cookieCart ,cartData, guestOrder
 
 
 def checkout(request):
@@ -49,35 +49,7 @@ def processOrder(request):
         )
 
     else:
-        print('User is not logged in...')  
-        print('COOKIES', request.COOKIES)
-        
-        name = data['form']['name']
-        email =  data['form']['email']
-
-        cookieData =  cookieCart(request)
-        items = cookieData['items']
-
-        customer, created = Customer.objects.get_or_create( 
-            email = email
-            )
-        
-        customer.name = name
-        customer.save()
-        
-        order =  Order.objects.create(
-            customer =  customer,
-            complete = False
-        )
-
-        for item in items:
-            product = Product.objects.get(id= item['product']['id'])
-
-            orderItem = OrderItem.objects.create(
-                product = product,
-                order = order,
-                quantity = item['quantity']
-            )
+        customer, order = guestOrder(request, data)
 
 
 
